7426293cbef95dc4ff8018abb497ad445e59e18e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-01-14 15:06:29 +0900

    chore(public): reuse external types

--- public/markets.ts
+++ public/markets.ts
@@ -1,70 +1,10 @@
 import { BASE_URL } from "./constants.ts";
 import { jsonFetch } from "./_utils.ts";
-
-type BaseMarketData = {
-  quoteVolume24h: number;
-
-  /** Change in the past hour */
-  change1h: number;
-
-  /** Change in the past 24 hours */
-  change24h: number;
-
-  /** Change since start of day (00:00 UTC) */
-  changeBod: number;
-
-  highLeverageFeeExempt: boolean;
-
-  /** Minimum maker order size (if >10 orders per hour fall below this size) */
-  minProvideSize: number;
-
-  enabled: boolean;
-
-  /** best ask */
-  ask: number | null;
-
-  /** best bid */
-  bid: number | null;
-
-  /** last traded price */
-  last: number | null;
-
-  /** current price */
-  price: number | null;
-
-  /** if the market is in post-only mode (all orders get modified to be post-only, in addition to other settings they may have) */
-  postOnly: boolean;
-
-  priceIncrement: number;
-  sizeIncrement: number;
-
-  /** if the market has nonstandard restrictions on which jurisdictions can trade it */
-  restricted: boolean;
-
-  /** USD volume in past 24 hours */
-  volumeUsd24h: number;
-  tokenizedEquity?: boolean;
-};
+import { MarketData } from "./market.ts";
 
 export type MarketsResponse = {
   success: true;
-  result: (
-    & BaseMarketData
-    & ({
-      /** e.g. `"BTC/USD"` */
-      name: `${string}/${string}`;
-      baseCurrency: string;
-      quoteCurrency: string;
-      underlying: null;
-      type: "spot";
-    } | {
-      name: string;
-      baseCurrency: null;
-      quoteCurrency: null;
-      underlying: string;
-      type: "future";
-    })
-  )[];
+  result: MarketData[];
 };
 
 /** This section covers all types of markets on FTX: spot, perpetual futures, expiring futures, and MOVE contracts.
